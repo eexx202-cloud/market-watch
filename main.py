b8a569908d94cf7f3f574c8f331c9140919bea78
eexx202-cloud <eexx202@gmail.com>
2026-06-11 12:42:09 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,16 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
-from bs4 import BeautifulSoup
-import requests
 import os
 
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
 
-        try:
-            url = "https://finance.naver.com/sise/"
+        msg = """
+KOSPI WATCH
 
-            headers = {
-                "User-Agent": "Mozilla/5.0"
-            }
+KOSDAQ WATCH
 
-            r = requests.get(url, headers=headers, timeout=10)
-
-            soup = BeautifulSoup(r.text, "html.parser")
-
-            kospi = soup.select_one("#KOSPI_now")
-            kosdaq = soup.select_one("#KOSDAQ_now")
-
-            kospi_text = kospi.text.strip() if kospi else "조회실패"
-            kosdaq_text = kosdaq.text.strip() if kosdaq else "조회실패"
-
-            msg = f"""
-KOSPI : {kospi_text}
-
-KOSDAQ : {kosdaq_text}
+Next Step Ready
 """
-
-        except Exception as e:
-            msg = f"ERROR : {e}"
 
         self.send_response(200)
         self.end_headers()
@@ -43,5 +23,3 @@ server = HTTPServer(("0.0.0.0", port), Handler)
 print(f"Server started on {port}")
 
 server.serve_forever()
-import requests
-from bs4 import BeautifulSoup
