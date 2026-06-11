53e493d41ae595a0d108ca9ba2e4a5cd4b75e77c
eexx202-cloud <eexx202@gmail.com>
2026-06-11 12:23:27 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(b"Market Watch Running")
+        self.wfile.write(b"KOSPI KOSDAQ WATCH")
 
 port = int(os.environ.get("PORT", 10000))
 
