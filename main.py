50c11ce5c515755d4bd7b2b13ba3098ce4739347
eexx202-cloud <eexx202@gmail.com>
2026-06-11 12:16:28 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-import time
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import os
 
-print("market-watch started")
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Market Watch Running")
 
-while True:
-    print("running...")
-    time.sleep(60)
+port = int(os.environ.get("PORT", 10000))
+
+server = HTTPServer(("0.0.0.0", port), Handler)
+
+print(f"Server started on {port}")
+
+server.serve_forever()
