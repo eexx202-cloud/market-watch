f7d18341b05a75330bcc8fe3be45e12635042d47
eexx202-cloud <eexx202@gmail.com>
2026-06-17 21:21:46 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,7 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
-import requests
-import os
-
-IP = requests.get("https://api.ipify.org").text
-
-class Handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        html = f"""
-        <html>
-        <body>
-        <h1>80억 프로젝트</h1>
-        <p>현재 Render IP : {IP}</p>
-        </body>
-        </html>
-        """
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(html.encode())
-
-port = int(os.environ.get("PORT", 10000))
-HTTPServer(("0.0.0.0", port), Handler).serve_forever()
+TOKEN_RESULT = f"""
+STATUS={r.status_code}
+<br><br>
+HEADERS={dict(r.headers)}
+<br><br>
+BODY={r.text}
+"""
