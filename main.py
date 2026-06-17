d06ee0007e63d54428d000ce8b3eeab22b640a84
eexx202-cloud <eexx202@gmail.com>
2026-06-17 20:51:12 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import os
+import requests
+
+CLIENT_ID = os.environ.get("TOSS_CLIENT_ID")
+CLIENT_SECRET = os.environ.get("TOSS_CLIENT_SECRET")
+
+TOKEN_RESULT = "아직 테스트 안함"
+
+try:
+    r = requests.post(
+        "https://openapi.tossinvest.com/oauth2/token",
+        json={
+            "grant_type": "client_credentials",
+            "client_id": CLIENT_ID,
+            "client_secret": CLIENT_SECRET
+        },
+        timeout=10
+    )
+
+    TOKEN_RESULT = f"STATUS={r.status_code}<br>{r.text[:500]}"
+
+except Exception as e:
+    TOKEN_RESULT = str(e)
 
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -7,9 +29,16 @@ class Handler(BaseHTTPRequestHandler):
         <html>
         <body>
         <h1>80억 프로젝트</h1>
+
         <p>ACCOUNT : {bool(os.environ.get('TOSS_ACCOUNT'))}</p>
         <p>CLIENT_ID : {bool(os.environ.get('TOSS_CLIENT_ID'))}</p>
         <p>CLIENT_SECRET : {bool(os.environ.get('TOSS_CLIENT_SECRET'))}</p>
+
+        <hr>
+
+        <h3>토큰 테스트</h3>
+        <p>{TOKEN_RESULT}</p>
+
         </body>
         </html>
         """
