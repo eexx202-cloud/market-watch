e499217bb515dfc7035f2c5cc64ecbc57bf683b4
eexx202-cloud <eexx202@gmail.com>
2026-06-17 20:36:33 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from datetime import datetime
 import os
 
 print("ACCOUNT:", bool(os.environ.get("TOSS_ACCOUNT")))
 print("CLIENT_ID:", bool(os.environ.get("TOSS_CLIENT_ID")))
 print("CLIENT_SECRET:", bool(os.environ.get("TOSS_CLIENT_SECRET")))
 
-while True:
-    pass
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        html = f"""
+        <html>
+        <body>
+        <h1>80억 프로젝트</h1>
+        <p>{datetime.now()}</p>
+        </body>
+        </html>
+        """
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(html.encode())
+
+port = int(os.environ.get("PORT", 10000))
+HTTPServer(("0.0.0.0", port), Handler).serve_forever()
