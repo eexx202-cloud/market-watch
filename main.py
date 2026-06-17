1cb83dc0bba3ad3bda6dd0068eead36a8ab2465b
eexx202-cloud <eexx202@gmail.com>
2026-06-17 20:35:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+import requests
 import os
 
-print("ACCOUNT:", bool(os.environ.get("TOSS_ACCOUNT")))
-print("CLIENT_ID:", bool(os.environ.get("TOSS_CLIENT_ID")))
-print("CLIENT_SECRET:", bool(os.environ.get("TOSS_CLIENT_SECRET")))
+CLIENT_ID = os.environ.get("TOSS_CLIENT_ID")
+CLIENT_SECRET = os.environ.get("TOSS_CLIENT_SECRET")
+
+print("토큰 발급 테스트 시작")
+
+r = requests.post(
+    "토스 API 토큰 주소",
+    json={
+        "grant_type": "client_credentials",
+        "client_id": CLIENT_ID,
+        "client_secret": CLIENT_SECRET
+    }
+)
+
+print(r.status_code)
+print(r.text[:500])
