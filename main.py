62a52d644fb6e47e27c09666cd386b1ffb0aadc6
eexx202-cloud <eexx202@gmail.com>
2026-06-17 21:38:45 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,39 +5,27 @@ import requests
 CLIENT_ID = os.environ.get("TOSS_CLIENT_ID")
 CLIENT_SECRET = os.environ.get("TOSS_CLIENT_SECRET")
 
-try:
-    r = requests.post(
-        "https://openapi.tossinvest.com/oauth2/token",
-        headers={
-            "Content-Type": "application/x-www-form-urlencoded"
-        },
-        data={
-            "grant_type": "client_credentials",
-            "client_id": CLIENT_ID,
-            "client_secret": CLIENT_SECRET
-        },
-        timeout=10
-    )
+r = requests.post(
+    "https://openapi.tossinvest.com/oauth2/token",
+    headers={
+        "Content-Type": "application/x-www-form-urlencoded"
+    },
+    data={
+        "grant_type": "client_credentials",
+        "client_id": CLIENT_ID,
+        "client_secret": CLIENT_SECRET
+    }
+)
 
-    TOKEN_RESULT = f"""
-    STATUS={r.status_code}
-    <br><br>
-    {r.text}
-    """
+RESULT = f"""
+STATUS={r.status_code}
 
-except Exception as e:
-    TOKEN_RESULT = str(e)
+{r.text}
+"""
 
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
-        html = f"""
-        <html>
-        <body>
-        <h1>80억 프로젝트</h1>
-        <pre>{TOKEN_RESULT}</pre>
-        </body>
-        </html>
-        """
+        html = f"<h1>80억 프로젝트</h1><pre>{RESULT}</pre>"
 
         self.send_response(200)
         self.send_header("Content-type", "text/html; charset=utf-8")
